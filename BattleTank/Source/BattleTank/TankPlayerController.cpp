10f0f71d854ae7df0db7e7eef8cda19986932e2e
outlawdev <skinnertx@gmail.com>
2019-07-29 20:24:58 -0500

    Setup the aim tracing function to allow for aim sight under crosshairs.

--- BattleTank/Source/BattleTank/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -33,4 +33,16 @@ ATank* ATankPlayerController::GetControlledTank() const
 void ATankPlayerController::AimTowardsCrosshair()
 {
     if (!GetControlledTank()) { return; }
+
+    FVector HitLocation;
+    if (GetSightRayHitLocation(HitLocation))
+    {
+        UE_LOG(LogTemp, Warning, TEXT("Hit location:  %s"), *HitLocation.ToString());
+    }
+}
+
+bool ATankPlayerController::GetSightRayHitLocation(FVector& HitLocation) const
+{
+    HitLocation = FVector(1.);
+    return true;
 }
