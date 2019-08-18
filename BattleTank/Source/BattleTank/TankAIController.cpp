de6a953f8f200b71066fee723323e96cef831ca9
outlawdev <skinnertx@gmail.com>
2019-08-17 21:01:10 -0500

    got to the challenge slide of the requestdirectmove() class...need to finish it.

--- BattleTank/Source/BattleTank/TankAIController.cpp
+++ BattleTank/Source/BattleTank/TankAIController.cpp
@@ -12,6 +12,8 @@ void ATankAIController::Tick(float DeltaTime)
 
     if (ControlledTank && PlayerTank)
     {
+        MoveToActor(PlayerTank, AcceptanceRadius);
+
         ControlledTank->AimAt(PlayerTank->GetActorLocation());
         ControlledTank->Fire();
     }
